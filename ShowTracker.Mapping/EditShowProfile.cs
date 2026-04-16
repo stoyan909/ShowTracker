e32f293825a28e01fc29bf8372fe753d9b21d4a0
stoyan909 <stoyan909@abv.bg>
2026-04-16 21:22:31 +0300

    Created and edit show profile

---
+++ ShowTracker.Mapping/EditShowProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using ShowTracker.Data.Models;
+using ShowTracker.ViewModel.ShowsViewModel;
+
+namespace ShowTracker.Mapping
+{
+    public class EditShowProfile : Profile
+    {
+        public EditShowProfile()
+        {
+            CreateMap<Show, EditShowViewModel>()
+                .ForMember(dest => dest.SeasonNumber, opt => opt.MapFrom(src => src.Seasons.Count()));
+
+            CreateMap<EditShowViewModel, Show>();
+
+        }
+    }
+}
