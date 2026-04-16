b9f2431385809be5b7838c65ec74ea3337c4e2bd
stoyan909 <stoyan909@abv.bg>
2026-04-16 21:24:12 +0300

    Created a helper class

---
+++ ShowTracker.Common/HelperMethods.cs
@@ -0,0 +1,11 @@
+﻿namespace ShowTracker.Common
+{
+    public static class HelperMethods
+    {
+        public static int GetSeasonDifference(int currentCount, int newCount)
+        {
+            return newCount - currentCount;
+        }
+
+    }
+}
